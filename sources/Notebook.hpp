0f13fefa15d3932e6582f3e973734be448f3fcbe
michael <michael.matve@gmail.com>
2022-03-25 19:35:55 +0300

    second

--- sources/Notebook.hpp
+++ sources/Notebook.hpp
@@ -1,15 +1,23 @@
 #include "Direction.hpp"
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace ariel{
     class Notebook{
         private:
-            const int maxcolum = 100;
-            std::vector<std::vector<std::vector<char>>> arr_book;
+            static const int maxcolum = 99;
+            std::unordered_map<int, std::unordered_map<int,std::vector<char>>>My_book;
+
+            void write_Horizontal(int page, int row, int colum, const std::string &word);
+            void write_Vertical(int page, int row, int colum, const std::string &word);
+            std::string read_Horizontal(int page, int row, int colum, int length);
+            std::string read_Vertical(int page, int row, int colum, int length);
+            void erase_Horizontal(int page, int row, int colum, int length);
+            void erase_Vertical(int page, int row, int colum, int length);
         public:
             void write(int page, int row, int colum, ariel::Direction dir, const std::string &word);
-            std::string static read(int page, int row, int colum, ariel::Direction dir, int length);
+            std::string read(int page, int row, int colum, ariel::Direction dir, int length);
             void erase(int page, int row, int colum, ariel::Direction dir, int length);
             void show(int page);
     };
